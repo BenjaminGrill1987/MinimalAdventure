cf8a3b12fb4af60c156540ac759baae00dce4981
BenjaminGrill1987 <benjamin.grill@web.de>
2026-02-22 20:22:59 +0100

    Place names

--- Assets/Scripts/BoilerPlate/Places.cs
+++ Assets/Scripts/BoilerPlate/Places.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public abstract class Places : MonoBehaviour
 {
+    [SerializeField] protected TextMeshProUGUI _placeNameSign;
     [SerializeField] protected GameObject _panel, _firstSelected;
+
+    protected string _placeName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
