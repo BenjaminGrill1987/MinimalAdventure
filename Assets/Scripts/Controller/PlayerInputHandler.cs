8ab72a9c3d333604ef39d28c322f06d33f123904
BenjaminGrill1987 <77078987+BenjaminGrill1987@users.noreply.github.com>
2026-02-22 19:01:15 +0100

    Add dedicated player input component and wire to player

---
+++ Assets/Scripts/Controller/PlayerInputHandler.cs
@@ -0,0 +1,32 @@
+﻿using MinimalAdventure.PlayerInput;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private CharacterInput _characterInput;
+    private InputAction _moveAction;
+
+    public Vector2 Move => _moveAction.ReadValue<Vector2>();
+
+    private void Awake()
+    {
+        _characterInput = new CharacterInput();
+        _moveAction = _characterInput.Player.Move;
+    }
+
+    private void OnEnable()
+    {
+        _characterInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _characterInput.Disable();
+    }
+
+    public bool MoveWasPressedThisFrame()
+    {
+        return _moveAction.WasPressedThisFrame();
+    }
+}
--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -1,28 +1,18 @@
-﻿using MinimalAdventure.PlayerInput;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(PlayerInputHandler))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] MoveAble moveable;
 
     private Vector3Int _pos;
-    private CharacterInput _characterController;
-    private InputAction _move;
+    private PlayerInputHandler _playerInput;
 
     private void Awake()
     {
-        _characterController = new CharacterInput();
+        _playerInput = GetComponent<PlayerInputHandler>();
         _pos = new Vector3Int(0, 0, 0);
         transform.position = _pos;
-    }
-
-    private void OnEnable()
-    {
-        _characterController.Enable();
-        _move = _characterController.Player.Move;
     }
 
     private void Update()
@@ -31,41 +21,45 @@ public class PlayerController : MonoBehaviour
         {
             InputUpdate();
         }
+
         MoveUpdate();
     }
 
     private void InputUpdate()
     {
-        if (_characterController.Player.Move.WasPressedThisFrame())
+        if (!_playerInput.MoveWasPressedThisFrame())
         {
+            return;
+        }
 
-            if (_move.ReadValue<Vector2>().y > 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x, _pos.y + 1, _pos.z))))
+        Vector2 moveInput = _playerInput.Move;
+
+        if (moveInput.y > 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x, _pos.y + 1, _pos.z))))
+        {
+            if (_pos.y < MapHandler.GetOverworldMap().GetLength(1) - 1)
             {
-                if (_pos.y < MapHandler.GetOverworldMap().GetLength(1) - 1)
-                {
-                    _pos.y += 1;
-                }
+                _pos.y += 1;
             }
-            else if (_move.ReadValue<Vector2>().y < 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x, _pos.y - 1, _pos.z))))
+        }
+        else if (moveInput.y < 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x, _pos.y - 1, _pos.z))))
+        {
+            if (_pos.y > 0)
             {
-                if (_pos.y > 0)
-                {
-                    _pos.y -= 1;
-                }
+                _pos.y -= 1;
             }
-            else if (_move.ReadValue<Vector2>().x > 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x + 1, _pos.y, _pos.z))))
+        }
+        else if (moveInput.x > 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x + 1, _pos.y, _pos.z))))
+        {
+            if (_pos.x < MapHandler.GetOverworldMap().GetLength(0) - 1)
             {
-                if (_pos.x < MapHandler.GetOverworldMap().GetLength(0) - 1)
-                {
-                    _pos.x += 1;
-                }
+                _pos.x += 1;
             }
-            else if (_move.ReadValue<Vector2>().x < 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x - 1, _pos.y, _pos.z))))
+        }
+        else if (moveInput.x < 0 && moveable.CanMove(MapDraw.GetOverWorldMapTile(new Vector3Int(_pos.x - 1, _pos.y, _pos.z))))
+        {
+            if (_pos.x > 0)
             {
-                if (_pos.x > 0)
-                {
-                    _pos.x -= 1;
-                }
+                _pos.x -= 1;
             }
         }
     }
